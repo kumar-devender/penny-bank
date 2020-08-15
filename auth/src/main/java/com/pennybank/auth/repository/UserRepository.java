682964ee274ce067727941dedadcbef3e354f763
devender.kumar <devender.kumar@auto1.com>
2020-08-15 12:09:02 +0200

    rename package

---
+++ auth/src/main/java/com/pennybank/auth/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.pennybank.auth.repository;
+
+import com.pennybank.auth.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+    Optional<UserEntity> findByUsername(String username);
+}
